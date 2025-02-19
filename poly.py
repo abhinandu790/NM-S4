daf1e74b71d626a0119f355cb98897a4ccab195d
ABHINANDANA T U <151870340+abhinandu790@users.noreply.github.com>
2025-02-19 14:39:50 +0530

    Update poly.py

--- poly.py
+++ poly.py
@@ -6,8 +6,6 @@ def forward_difference(x, h):
 
 x = 2
 h = 0.1
-approx_derivative = forward_difference(x, h)
+first_derivative = forward_difference(x, h)
 
-print(f"Approximate first derivative at x={x} using h={h}: {approx_derivative}")
-
-
+print(f"First derivative of x**3 + 2*x**2 - 5*x + 7 :{first_derivative}")
